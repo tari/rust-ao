a5ea1474b00b1f102ccc101920eca3e4cad305b4
Peter Marheine <peter@taricorp.net>
2014-07-15 17:12:38 -0400

    Track Rust master.

--- src/ffi.rs
+++ src/ffi.rs
@@ -15,38 +15,46 @@ extern "C" {
     pub fn ao_initialize();
     pub fn ao_shutdown();
 
-    pub fn ao_driver_id(short_name: *c_char) -> c_int;
+    pub fn ao_driver_id(short_name: *const c_char) -> c_int;
     pub fn ao_default_driver_id() -> c_int;
 
-    pub fn ao_driver_info(driver_id: c_int) -> *ao_info;
+    pub fn ao_driver_info(driver_id: c_int) -> *const ao_info;
     
-    pub fn ao_append_option(options: **ao_option, key: *c_char, value: *c_char) -> c_int;
+    pub fn ao_append_option(options: *mut *mut ao_option,
+                            key: *const c_char,
+                            value: *const c_char) -> c_int;
 
     pub fn ao_open_live(driver_id: c_int,
-                    format: *ao_sample_format, options: *ao_option) -> *ao_device;
-    pub fn ao_open_file(driver_id: c_int, filename: *c_char, overwrite: c_int,
-                    format: *ao_sample_format, options: *ao_option) -> *ao_device;
+                        format: *const ao_sample_format,
+                        options: *const ao_option) -> *mut ao_device;
+    pub fn ao_open_file(driver_id: c_int,
+                        filename: *const c_char,
+                        overwrite: c_int,
+                        format: *const ao_sample_format,
+                        options: *const ao_option) -> *mut ao_device;
 
-    pub fn ao_close(device: *ao_device) -> c_int;
+    pub fn ao_close(device: *mut ao_device) -> c_int;
 
-    pub fn ao_play(device: *ao_device, output_samples: *c_char, num_bytes: u32) -> c_int;
+    pub fn ao_play(device: *mut ao_device,
+                   output_samples: *const c_char,
+                   num_bytes: u32) -> c_int;
 }
 
 pub struct ao_info {
     flavor: c_int,
-    name: *c_char,
-    short_name: *c_char,
-    comment: *c_char,
+    name: *mut c_char,
+    short_name: *mut c_char,
+    comment: *mut c_char,
     preferred_byte_format: c_int,
     priority: c_int,
-    options: **c_char,
+    options: *mut *mut c_char,
     option_count: c_int,
 }
 
 pub struct ao_option {
-    key: *c_char,
-    value: *c_char,
-    next: *ao_option
+    key: *mut c_char,
+    value: *mut c_char,
+    next: *mut ao_option
 }
 
 // Opaque struct
@@ -57,7 +65,7 @@ pub struct ao_sample_format {
     pub rate: c_int,
     pub channels: c_int,
     pub byte_format: c_int,
-    pub matrix: *c_char
+    pub matrix: *const c_char
 }
 
 pub static AO_FMT_LITTLE: int = 1;
